f768a0acfdee28cc26dc3cb9cbcf6bf54a3ba355
Sardorbek <smakhamadaliev8@gmail.com>
2023-04-02 12:26:50 +0200

    sing in page validation and other changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import "./styles/App.scss";
 import Navigator from "./components/Navigation";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "@fortawesome/fontawesome-free/js/all.min.js";
-import { Route, Routes} from "react-router-dom";
-import SingInPage from "./components/SignInPage";
+import { Route, Switch } from "react-router-dom";
+import SignInPage from "./components/SignInPage";
+import MoviesPage from "./components/MoviesPage/MoviesPage";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={
-          <Navigator />
-        }
-        />
-
-        <Route path="/sign-in" element={
-          <SingInPage />
-        }
-        />
-      </Routes>
+    <Switch>
+      {" "}
+      <Route exact path="/">
+        {" "}
+        <Navigator />{" "}
+      </Route>{" "}
+      <Route path="/sign-in">
+        {" "}
+        <SignInPage />{" "}
+      </Route>{" "}
+      <Route exact path="/movies">
+        {" "}
+        <MoviesPage />{" "}
+      </Route>{" "}
+    </Switch>
   );
 }
 
--- src/components/SignInPage/SignInPage.jsx
+++ src/components/SignInPage/SignInPage.jsx
@@ -1,9 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 import "../../styles/App.scss";
+import { withRouter } from "react-router";
 import Footer from "../Footer";
 
-class SingInPage extends React.Component {
+class SignInPage extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    passwordError: "",
+  };
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+      passwordError: "",
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email, password } = this.state;
+    if (password.length < 8 || !/\d/.test(password)) {
+      this.setState({
+        passwordError:
+          "Password must be at least 8 characters long and contain a number",
+      });
+      return;
+    }
+    // submit the form
+    console.log(`Email: ${email}, Password: ${password}`);
+  
+    if (email !== " ") {
+      // Redirect to the main page
+      this.props.history.push("/movies");
+    } else {
+      alert("Invalid email or password");
+    }
+  };
+
   render() {
+    const { email, password, passwordError } = this.state;
     return (
       <div className="signing-page">
         <div className="signing-page__wrapper">
@@ -19,7 +56,38 @@ class SingInPage extends React.Component {
           <div className="signing-page__form-wrapper">
             <div className="signing-page__form-body">
               <h2 className="signing-page__form-body-header">Sign In</h2>
-              <form action="/" method="get" className="signing-page__form">
+              <form
+                action="/"
+                method="get"
+                className="signing-page__form"
+                onSubmit={this.handleSubmit}
+              >
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={email}
+                    required
+                    onChange={this.handleInputChange}
+                  />
+                </div>
+                <div>
+                  <label htmlFor="password">Password:</label>
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    required
+                    value={password}
+                    onChange={this.handleInputChange}
+                  />
+                  {passwordError && (
+                    <div className="error">{passwordError}</div>
+                  )}
+                </div>
+                <button type="submit">Sign In</button>
               </form>
             </div>
           </div>
@@ -30,4 +98,4 @@ class SingInPage extends React.Component {
   }
 }
 
-export default SingInPage;
+export default withRouter(SignInPage);
